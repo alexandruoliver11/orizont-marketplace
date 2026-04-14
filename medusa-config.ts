1716b796de30927ba0d4a4c70db1829acd684d7f
ChipicaoDEV <realoilcoin@gmail.com>
2026-04-14 20:28:39 +0300

    f9ixe

--- medusa-config.ts
+++ medusa-config.ts
@@ -16,9 +16,6 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      cookieOptions: {
-        secure: process.env.NODE_ENV === "production",
-      },
     },
   },
   admin: {
